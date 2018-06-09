25a7a23e7e515f8f0ce092b847a8ff1d01dad83a
RajuShetty <rajushetty.b@gmail.com>
2018-06-09 21:48:10 +0530

    testing

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'sharon-adugegalu',
+		wp_ws_url : 'http://kannadachristiansongs.in/test-recipes/wp-appkit-api/sharon-adugegalu',
+		wp_url : 'http://kannadachristiansongs.in/test-recipes',
+		theme : 'q-android',
+		version : '1',
+		app_type : 'phonegap-build',
+		app_title : 'Sharon Adugegalu',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'K>6U+~Bq,m*FSpN+1f~QP}/f}8KI9uUHbx}y;;`[I2y#99X[f}z-:o,7]A,B,$34',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
