ba46b916623521585dbe1562e9f410f43d2e94aa
RajuShetty <rajushetty.b@gmail.com>
2018-07-03 03:05:03 +0530

    search added

--- themes/q-android-feat-favorites/js/functions.js
+++ themes/q-android-feat-favorites/js/functions.js
@@ -962,3 +962,61 @@ define([
     }
     
 });
+/**
+ * Memorize current search so that it is always available,
+ * even after changing screen
+ */
+var current_search = {
+    search_string: ''
+};
+
+$( '#app-layout' ).on( 'click', '#search-button', function( e ) {
+    e.preventDefault();
+
+    //Set search params from HTML form:
+    current_search.search_string = $('#search-field').val().trim();
+
+    //Get updated data from server for the current component:
+    App.refreshComponent({
+        success: function( answer, update_results ) {
+            //Server answered with a filtered list of posts. 
+            //Reload current screen to see the result:
+            App.reloadCurrentScreen();
+        },
+        error: function( error ) {
+            //Maybe do something if filtering went wrong.
+            //Note that "No network" error events are triggered automatically by core
+        }
+    });
+
+} );
+/**
+ * Add our search params to web services that retrieve our post list.
+ * Applies to "Live Query" web service (that retrieves filtered component's post list)
+ * and to "Get More Posts" web service (so that search filters apply to pagination too).
+ */
+App.filter( 'web-service-params', function( web_service_params ) {
+ 
+    //If the user provided non empty search params:
+    if( current_search.search_string !== '' ) {
+        //Add search params to the data sent to web service:
+        web_service_params.my_search_filters = current_search;
+        //Those params will be retrieved with WpakWebServiceContext::getClientAppParam( 'my_search_filters' )
+        //on server side.
+    }
+ 
+    return web_service_params;
+} );
+
+/**
+ * Add 
+ * - current search params to the archive template, so that they're available in archive.html.
+ */
+App.filter( 'template-args', function( template_args, view_type, view_template ) {
+
+    if ( view_type === 'archive' ) {
+        template_args.current_search = current_search;
+    }
+
+    return template_args;
+} );
